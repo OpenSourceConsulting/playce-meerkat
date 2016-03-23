861dccfa53780e95de4b5c68eb19581e6f61776b
nices96 <nices96@gmail.com>
2016-03-23 19:46:13 +0900

    Load domain info, Edit domain, load tomcat list in domain, load domain tomcat config, load datasource list

--- controller/src/main/java/com/athena/meerkat/controller/web/entities/DomainTomcatConfiguration.java
+++ controller/src/main/java/com/athena/meerkat/controller/web/entities/DomainTomcatConfiguration.java
@@ -1,266 +1,209 @@
-/**
- * 
- */
 package com.athena.meerkat.controller.web.entities;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 /**
- * @author Tran
- *
+ * <pre>
+ * 
+ * </pre>
+ * 
+ * @author Tran Ho
+ * @version 2.0
  */
 @Entity
-@Table
-public class DomainTomcatConfiguration {
-	@Id
-	private int id;
-	private int domainId;
-	private String javaHome;
-	private String catalinaHome;
-	private String catalinaBase;
-	private int httpPort;
-	private int sessionTimeout;
-	private int ajpPort;
-	private int redirectPort;
-	private boolean jmxEnable;
-	private int rmiRegistryPort;
-	private int rmiServerPort;
-	private String catalinaOpts;
-	private int modifiedUserId;
-	private java.sql.Date modifiedDate;
+@Table(name = "domain_tomcat_configuration")
+public class DomainTomcatConfiguration implements Serializable {
 
 	/**
 	 * 
 	 */
-	public DomainTomcatConfiguration() {
-		// TODO Auto-generated constructor stub
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Id")
+	private int Id;
+	@Column(name = "java_home")
+	private String javaHome;
+	@Column(name = "catalina_home")
+	private String catalinaHome;
+	@Column(name = "catalina_base")
+	private String catalinaBase;
+	@Column(name = "http_port")
+	private int httpPort;
+	@Column(name = "ajp_port")
+	private int ajpPort;
+	@Column(name = "session_timeout")
+	private int sessionTimeout;
+	@Column(name = "redirect_port")
+	private int redirectPort;
+	@Column(name = "jmx_enable")
+	private boolean jmxEnable;
+	@Column(name = "rmi_registry_port")
+	private int rmiRegistryPort;
+	@Column(name = "rmi_server_port")
+	private int rmiServerPort;
+	@Column(name = "catalina_opts")
+	private String catalinaOpts;
+	@Column(name = "modified_user_id")
+	private int modifiedUserId;
+	@Column(name = "modified_date")
+	private Date modifiedDate;
+	@Column(name = "java_version")
+	private String javaVersion;
+	@Column(name = "encoding")
+	private String encoding;
+
+	@OneToOne
+	@JoinColumn(name = "domain_id")
+	@JsonBackReference
+	private TomcatDomain tomcatDomain;
+
+	public TomcatDomain getTomcatDomain() {
+		return tomcatDomain;
 	}
 
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
+	public void setTomcatDomain(TomcatDomain tomcatDomain) {
+		this.tomcatDomain = tomcatDomain;
 	}
 
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
+	public Date getModifiedDate() {
+		return modifiedDate;
 	}
 
-	/**
-	 * @return the domainId
-	 */
-	public int getDomainId() {
-		return domainId;
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
 	}
 
-	/**
-	 * @param domainId
-	 *            the domainId to set
-	 */
-	public void setDomainId(int domainId) {
-		this.domainId = domainId;
-	}
-
-	/**
-	 * @return the javaHome
-	 */
-	public String getJavaHome() {
-		return javaHome;
-	}
-
-	/**
-	 * @param javaHome
-	 *            the javaHome to set
-	 */
-	public void setJavaHome(String javaHome) {
-		this.javaHome = javaHome;
-	}
-
-	/**
-	 * @return the catalinaHome
-	 */
-	public String getCatalinaHome() {
-		return catalinaHome;
-	}
-
-	/**
-	 * @param catalinaHome
-	 *            the catalinaHome to set
-	 */
-	public void setCatalinaHome(String catalinaHome) {
-		this.catalinaHome = catalinaHome;
-	}
-
-	/**
-	 * @return the catalinaBase
-	 */
-	public String getCatalinaBase() {
-		return catalinaBase;
-	}
-
-	/**
-	 * @param catalinaBase
-	 *            the catalinaBase to set
-	 */
-	public void setCatalinaBase(String catalinaBase) {
-		this.catalinaBase = catalinaBase;
-	}
-
-	/**
-	 * @return the httpPort
-	 */
-	public int getHttpPort() {
-		return httpPort;
-	}
-
-	/**
-	 * @param httpPort
-	 *            the httpPort to set
-	 */
-	public void setHttpPort(int httpPort) {
-		this.httpPort = httpPort;
-	}
-
-	/**
-	 * @return the sessionTimeout
-	 */
-	public int getSessionTimeout() {
-		return sessionTimeout;
-	}
-
-	/**
-	 * @param sessionTimeout
-	 *            the sessionTimeout to set
-	 */
-	public void setSessionTimeout(int sessionTimeout) {
-		this.sessionTimeout = sessionTimeout;
-	}
-
-	/**
-	 * @return the ajpPort
-	 */
-	public int getAjpPort() {
-		return ajpPort;
-	}
-
-	/**
-	 * @param ajpPort
-	 *            the ajpPort to set
-	 */
-	public void setAjpPort(int ajpPort) {
-		this.ajpPort = ajpPort;
-	}
-
-	/**
-	 * @return the redirectPort
-	 */
-	public int getRedirectPort() {
-		return redirectPort;
-	}
-
-	/**
-	 * @param redirectPort
-	 *            the redirectPort to set
-	 */
-	public void setRedirectPort(int redirectPort) {
-		this.redirectPort = redirectPort;
-	}
-
-	/**
-	 * @return the jmxEnable
-	 */
-	public boolean getJmxEnable() {
-		return jmxEnable;
-	}
-
-	/**
-	 * @param jmxEnable
-	 *            the jmxEnable to set
-	 */
-	public void setJmxEnable(boolean jmxEnable) {
-		this.jmxEnable = jmxEnable;
-	}
-
-	/**
-	 * @return the rmiRegistryPort
-	 */
-	public int getRmiRegistryPort() {
-		return rmiRegistryPort;
-	}
-
-	/**
-	 * @param rmiRegistryPort
-	 *            the rmiRegistryPort to set
-	 */
-	public void setRmiRegistryPort(int rmiRegistryPort) {
-		this.rmiRegistryPort = rmiRegistryPort;
-	}
-
-	/**
-	 * @return the rmiServerPort
-	 */
-	public int getRmiServerPort() {
-		return rmiServerPort;
-	}
-
-	/**
-	 * @param rmiServerPort
-	 *            the rmiServerPort to set
-	 */
-	public void setRmiServerPort(int rmiServerPort) {
-		this.rmiServerPort = rmiServerPort;
-	}
-
-	/**
-	 * @return the catalinaOpts
-	 */
-	public String getCatalinaOpts() {
-		return catalinaOpts;
-	}
-
-	/**
-	 * @param catalinaOpts
-	 *            the catalinaOpts to set
-	 */
-	public void setCatalinaOpts(String catalinaOpts) {
-		this.catalinaOpts = catalinaOpts;
-	}
-
-	/**
-	 * @return the modifiedUserId
-	 */
 	public int getModifiedUserId() {
 		return modifiedUserId;
 	}
 
-	/**
-	 * @param modifiedUserId
-	 *            the modifiedUserId to set
-	 */
 	public void setModifiedUserId(int modifiedUserId) {
 		this.modifiedUserId = modifiedUserId;
 	}
 
-	/**
-	 * @return the modifiedDate
-	 */
-	public java.sql.Date getModifiedDate() {
-		return modifiedDate;
+	public String getCatalinaOpts() {
+		return catalinaOpts;
 	}
 
-	/**
-	 * @param modifiedDate
-	 *            the modifiedDate to set
-	 */
-	public void setModifiedDate(java.sql.Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
+	public void setCatalinaOpts(String catalinaOpts) {
+		this.catalinaOpts = catalinaOpts;
 	}
 
+	public int getRmiServerPort() {
+		return rmiServerPort;
+	}
+
+	public void setRmiServerPort(int rmiServerPort) {
+		this.rmiServerPort = rmiServerPort;
+	}
+
+	public int getRmiRegistryPort() {
+		return rmiRegistryPort;
+	}
+
+	public void setRmiRegistryPort(int rmiRegistryPort) {
+		this.rmiRegistryPort = rmiRegistryPort;
+	}
+
+	public boolean isJmxEnable() {
+		return jmxEnable;
+	}
+
+	public void setJmxEnable(boolean jmxEnable) {
+		this.jmxEnable = jmxEnable;
+	}
+
+	public int getRedirectPort() {
+		return redirectPort;
+	}
+
+	public void setRedirectPort(int redirectPort) {
+		this.redirectPort = redirectPort;
+	}
+
+	public int getSessionTimeout() {
+		return sessionTimeout;
+	}
+
+	public void setSessionTimeout(int sessionTimeout) {
+		this.sessionTimeout = sessionTimeout;
+	}
+
+	public int getAjpPort() {
+		return ajpPort;
+	}
+
+	public void setAjpPort(int ajpPort) {
+		this.ajpPort = ajpPort;
+	}
+
+	public int getHttpPort() {
+		return httpPort;
+	}
+
+	public void setHttpPort(int httpPort) {
+		this.httpPort = httpPort;
+	}
+
+	public String getCatalinaBase() {
+		return catalinaBase;
+	}
+
+	public void setCatalinaBase(String catalinaBase) {
+		this.catalinaBase = catalinaBase;
+	}
+
+	public String getCatalinaHome() {
+		return catalinaHome;
+	}
+
+	public void setCatalinaHome(String catalinaHome) {
+		this.catalinaHome = catalinaHome;
+	}
+
+	public String getJavaHome() {
+		return javaHome;
+	}
+
+	public void setJavaHome(String javaHome) {
+		this.javaHome = javaHome;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getJavaVersion() {
+		return javaVersion;
+	}
+
+	public void setJavaVersion(String javaVersion) {
+		this.javaVersion = javaVersion;
+	}
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
 }
