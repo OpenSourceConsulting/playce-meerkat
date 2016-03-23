6a8098fce1ce257ccf2eac32381870146274f78e
nices96 <nices96@gmail.com>
2016-03-23 19:45:43 +0900

    Load domain info, Edit domain, load tomcat list in domain, load domain tomcat config, load datasource list

--- controller/src/main/java/com/athena/meerkat/controller/web/entities/ClusteringConfiguration.java
+++ controller/src/main/java/com/athena/meerkat/controller/web/entities/ClusteringConfiguration.java
@@ -1,138 +1,103 @@
-/**
- * 
- */
 package com.athena.meerkat.controller.web.entities;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 /**
- * @author Tran
- *
+ * <pre>
+ * 
+ * </pre>
+ * 
+ * @author Tran Ho
+ * @version 2.0
  */
 @Entity
 @Table(name = "clustering_configuration")
-public class ClusteringConfiguration {
-	@Id
-	private int id;
-	private String name;
-	private String value;
-	private java.sql.Date createdTime;
-	private int datagridServerGroupId;
-	private int domainId;
-	private int version;
+public class ClusteringConfiguration implements Serializable {
 
 	/**
 	 * 
 	 */
-	public ClusteringConfiguration() {
-		// TODO Auto-generated constructor stub
-	}
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Id")
+	private int Id;
 
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
-	}
+	@Column(name = "name")
+	private String name;
+	@Column(name = "value")
+	private String value;
+	@Column(name = "created_time")
+	private Date createdTime;
+	@Column(name = "version")
+	private int version;
+	@ManyToOne
+	@JsonBackReference
+	private DatagridServerGroup datagridServerGroup;
 
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
+	@ManyToOne
+	@JsonBackReference
+	@JoinColumn(name = "domain_id")
+	private TomcatDomain tomcatDomain;
 
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the value
-	 */
 	public String getValue() {
 		return value;
 	}
 
-	/**
-	 * @param value
-	 *            the value to set
-	 */
 	public void setValue(String value) {
 		this.value = value;
 	}
 
-	/**
-	 * @return the createdTime
-	 */
-	public java.sql.Date getCreatedTime() {
+	public Date getCreatedTime() {
 		return createdTime;
 	}
 
-	/**
-	 * @param createdTime
-	 *            the createdTime to set
-	 */
-	public void setCreatedTime(java.sql.Date createdTime) {
+	public void setCreatedTime(Date createdTime) {
 		this.createdTime = createdTime;
 	}
 
-	/**
-	 * @return the datagridServerGroupId
-	 */
-	public int getDatagridServerGroupId() {
-		return datagridServerGroupId;
-	}
-
-	/**
-	 * @param datagridServerGroupId
-	 *            the datagridServerGroupId to set
-	 */
-	public void setDatagridServerGroupId(int datagridServerGroupId) {
-		this.datagridServerGroupId = datagridServerGroupId;
-	}
-
-	/**
-	 * @return the domainId
-	 */
-	public int getDomainId() {
-		return domainId;
-	}
-
-	/**
-	 * @param domainId
-	 *            the domainId to set
-	 */
-	public void setDomainId(int domainId) {
-		this.domainId = domainId;
-	}
-
-	/**
-	 * @return the version
-	 */
 	public int getVersion() {
 		return version;
 	}
 
-	/**
-	 * @param version
-	 *            the version to set
-	 */
 	public void setVersion(int version) {
 		this.version = version;
+	}
+
+	public DatagridServerGroup getDatagridServerGroup() {
+		return datagridServerGroup;
+	}
+
+	public void setDatagridServerGroup(DatagridServerGroup datagridServerGroup) {
+		this.datagridServerGroup = datagridServerGroup;
+	}
+
+	public TomcatDomain getTomcatDomain() {
+		return tomcatDomain;
+	}
+
+	public void setTomcatDomain(TomcatDomain tomcatDomain) {
+		this.tomcatDomain = tomcatDomain;
 	}
 
 }
