7d9d898c2de9fe0b98cb8b3cf920fa2ca926f9e2
idkbj <idkbj@osci.kr>
2016-04-01 10:40:49 +0900

    set spring boot contextPath

--- controller/src/main/java/com/athena/meerkat/controller/web/entities/TomcatInstance.java
+++ controller/src/main/java/com/athena/meerkat/controller/web/entities/TomcatInstance.java
@@ -67,11 +67,7 @@ public class TomcatInstance implements Serializable {
 
 	@Column(name = "state")
 	private int state = 5; //default value.
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> branch 'master' of https://github.com/OpenSourceConsulting/athena-meerkat.git
 	@Transient
 	private String stateNm;
 
