af3965f4581b7fc169f30a9ce5bfbbedc0e4d806
idkbj <idkbj@osci.kr>
2016-04-07 13:20:19 +0900

    add provision log window

--- controller/src/main/java/com/athena/meerkat/controller/web/provisioning/TomcatProvisioningController.java
+++ controller/src/main/java/com/athena/meerkat/controller/web/provisioning/TomcatProvisioningController.java
@@ -58,7 +58,7 @@ public class TomcatProvisioningController {
 	@ResponseBody
 	public SimpleJsonResponse install(@PathVariable("domainId") int domainId) {
 		
-		proviService.installTomcatInstance(domainId);
+		proviService.installTomcatInstance2(domainId);
 		//System.out.println("call install "+ domainId + "******************************");
 		
 		return new SimpleJsonResponse();
