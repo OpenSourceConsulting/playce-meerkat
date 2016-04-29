9f5da231556ab22e51f50a15a557567e89074f0e
idkbj <idkbj@osci.kr>
2016-04-29 18:34:45 +0900

    add jmx monitoring currentThreadCount

--- agent/src/main/java/com/athena/meerkat/agent/monitoring/jobs/TInstanceMonScheduledTask.java
+++ agent/src/main/java/com/athena/meerkat/agent/monitoring/jobs/TInstanceMonScheduledTask.java
@@ -1,7 +1,17 @@
 package com.athena.meerkat.agent.monitoring.jobs;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.ConnectException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
 
 import org.hyperic.sigar.NetConnection;
 import org.slf4j.Logger;
@@ -9,7 +19,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import com.athena.meerkat.agent.monitoring.utils.JSONUtil;
 import com.athena.meerkat.agent.monitoring.utils.SigarUtil;
 import com.athena.meerkat.agent.monitoring.websocket.StompWebSocketClient;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -32,6 +44,8 @@ public class TInstanceMonScheduledTask extends MonitoringTask{
     
     private List<String> monDatas = new ArrayList<String>();
     
+    private Map<String, JMXConnector> jmxConnMap = new HashMap<String, JMXConnector>();
+    
     
     @Autowired
     private StompWebSocketClient webSocketClient;
@@ -47,20 +61,19 @@ public class TInstanceMonScheduledTask extends MonitoringTask{
     	
 		monDatas.clear();
     	try{
-    		ArrayNode configs = webSocketClient.getInstanceConfigs();
+    		ArrayNode tomcatConfigs = webSocketClient.getInstanceConfigs();//DomainTomcatConfiguration array
     		
-    		if (configs == null) {
-    			LOGGER.debug("tomcat instance configs is null. monitoring skip!");
+    		if (tomcatConfigs == null || tomcatConfigs.size() == 0) {
+    			LOGGER.debug("tomcat instance configs is empty. monitoring skip!");
     			return;
 			}
     		
     		NetConnection[] listenPorts = SigarUtil.getListenPorts();
     		
-    		
-    		for (JsonNode jsonNode : configs) {
+    		for (JsonNode tomcatConfig : tomcatConfigs) {
     			
-    			String tomcatInstanceId = jsonNode.get("tomcatInstanceId").asText();
-    			long port = jsonNode.get("httpPort").asLong();
+    			String tomcatInstanceId = tomcatConfig.get("tomcatInstanceId").asText();
+    			long port = tomcatConfig.get("httpPort").asLong();
     			int isRun = 7; // TS_STATE common code id.
     			for (NetConnection netConnection : listenPorts) {
     				if (netConnection.getLocalPort() == port) {
@@ -69,6 +82,7 @@ public class TInstanceMonScheduledTask extends MonitoringTask{
     			}
     			
     			monDatas.add(createJmxJsonString("ti.run", tomcatInstanceId, isRun));//tomcat instance running status.
+    			monitorJMX(monDatas, tomcatInstanceId, tomcatConfig);
     		}
     		
     		//TODO add another jmx monitoring.
@@ -81,6 +95,51 @@ public class TInstanceMonScheduledTask extends MonitoringTask{
     		monDatas.clear();
     	}
     }
+	
+	private void monitorJMX(List<String> monDatas, String tomcatInstanceId, JsonNode tomcatConfig) {
+		
+		String rmiRegistryPort 	= JSONUtil.getString(tomcatConfig, "rmiRegistryPort");
+		String httpPort			= JSONUtil.getString(tomcatConfig, "httpPort");
+		
+		if (StringUtils.isEmpty(rmiRegistryPort)) {
+			
+			LOGGER.debug("rmi port is empty. monitoring skip.");
+			return;
+		}
+		
+		JMXConnector jmxc = jmxConnMap.get(tomcatInstanceId);
+		
+		try{
+			if (jmxc == null) {
+				JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:"+ rmiRegistryPort +"/jmxrmi");
+				jmxc = JMXConnectorFactory.connect(url);
+				LOGGER.debug("JMX connected!!");
+				
+				jmxConnMap.put(tomcatInstanceId, jmxc);
+			}
+			
+			ObjectName name = new ObjectName("Catalina:type=ThreadPool,name=\"http-bio-"+ httpPort +"\"");
+			Object o = jmxc.getMBeanServerConnection().getAttribute(name, "currentThreadsBusy");// currentThreadCount or currentThreadsBusy
+			
+			monDatas.add(createJmxJsonString("jmx.currentThreadsBusy", tomcatInstanceId, Double.parseDouble(o.toString())));
+			
+		}catch(ConnectException e){
+			try{
+				jmxc.close();
+			}catch(IOException ex){
+				//ignore.
+			}
+			jmxConnMap.remove(tomcatInstanceId);
+			
+		}catch(Exception e){
+			//if (LOGGER.isDebugEnabled()) {
+			//	LOGGER.error(e.toString(), e);
+			//}else {
+				LOGGER.error("instanceId:{} - {}", tomcatInstanceId, e.toString());
+			//}
+		}
+		
+	}
     
     
 }
--- controller/db_alter_script.sql
+++ controller/db_alter_script.sql
@@ -7,7 +7,7 @@ CHANGE COLUMN `catalina_base` `catalina_base` VARCHAR(100) NOT NULL ;
 
 
 CREATE TABLE `mon_data_tbl` (
-  `mon_factor_id` varchar(10) NOT NULL,
+  `mon_factor_id` varchar(20) NOT NULL,
   `server_id` INT(11) NOT NULL,
   `mon_dt` datetime NOT NULL,
   `mon_value` double NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE `mon_data_tbl` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `mon_jmx_tbl` (
-  `mon_factor_id` varchar(10) NOT NULL,
+  `mon_factor_id` varchar(40) NOT NULL,
   `instance_id` INT(11) NOT NULL,
   `mon_dt` datetime NOT NULL,
   `mon_value` double NOT NULL,
@@ -41,3 +41,6 @@ CHANGE COLUMN `cd_desc` `cd_desc` VARCHAR(100) NULL DEFAULT NULL COMMENT '코드
 ALTER TABLE `server` 
 CHANGE COLUMN `disk_size` `disk_size` INT(11) NULL DEFAULT NULL ,
 CHANGE COLUMN `memory_size` `memory_size` INT(11) NULL DEFAULT NULL ;
+
+ALTER TABLE `mon_jmx_tbl` 
+CHANGE COLUMN `mon_factor_id` `mon_factor_id` VARCHAR(40) NOT NULL ;
