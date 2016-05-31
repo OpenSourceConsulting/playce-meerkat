8bf463b615df562f0ba4fc500bf687045e7ceb6a
nices96 <nices96@gmail.com>
2016-05-31 15:21:44 +0900

    merging

--- agent/runTarget.bat
+++ agent/runTarget.bat
@@ -9,4 +9,12 @@ set ANT_HOME=%~dp0apache-ant-1.9.6
 echo ANT_HOME: %ANT_HOME%
 
 
-%ANT_HOME%\bin\ant -buildfile %1 %2
+set "MRUN_OPTS=%1"
+
+if not "%2" == "" set "MRUN_OPTS=%MRUN_OPTS% %2"
+
+if not "%3" == "" set "MRUN_OPTS=%MRUN_OPTS% %3"
+
+echo ant run %MRUN_OPTS%
+
+%ANT_HOME%\bin\ant -buildfile %MRUN_OPTS%
--- agent/src/main/resources/bin/runCmd.sh
+++ agent/src/main/resources/bin/runCmd.sh
@@ -2,4 +2,9 @@
 
 . ./agentenv.sh
 
+if [ e$1 != "e" ] ; then
+    $ANT_HOME/bin/ant -buildfile $1 cmd
+    exit;
+fi
+
 $ANT_HOME/bin/ant -buildfile cmd.xml cmd
